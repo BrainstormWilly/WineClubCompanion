b79d8b574d42ecba8449a6b4617a0b4fc3e08629
Bill Langley <brainstormwilly@gmail.com>
2016-11-16 23:02:40 -0800

    bonsai add

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,5 +1,5 @@
 require 'elasticsearch/model'
 
 if ENV['BONSAI_ROSE_URL']
-    Elasticsearch::Model.client = Elasticsearch::Clien.new({url: ENV['BONSAI_ROSE_URL'], logs: true})
+    Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_ROSE_URL'], logs: true})
   end
